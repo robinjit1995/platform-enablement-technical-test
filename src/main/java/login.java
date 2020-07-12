3a8f7e30200313606f90205a5424050579e0384b
gagandeep singh <gaganminhas@gagans-Air.modem>
2020-07-12 18:21:43 +1000

    Alighned the folder heirarchy along maven

---
+++ src/main/java/login.java
@@ -0,0 +1,32 @@
+package com.myob.app;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet("/login")
+public class login extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String uname = request.getParameter("uname");
+		
+		if(uname.equals("Robinjit"))
+		{
+			response.sendRedirect("ApplicationHealth.jsp");
+			
+		}
+		else
+		{
+			response.sendRedirect("index.jsp");
+		}
+	}
+
+	
+
+}
---
+++ src/test/java/logintest.java
@@ -0,0 +1,14 @@
+package com.myob.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class logintest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
